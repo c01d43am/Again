6e34b386edf2c6da857bbf8126431c0bab773110
A Pawan <alugolupawan.com>
2025-01-20 13:02:08 +0530

    letgo

--- Again/index.py
+++ Again/index.py
@@ -20,7 +20,7 @@ def display_banner():
 ╚═╝░░╚═╝░╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░░░╚═╝░░
     """)
     # Display version at the bottom left corner
-    version = f"{green}\t\t\tv0.0.5 {reset}"
+    version = f"{green}\t\t\tv0.0.6 {reset}"
     print(f"\n{version}\n")  # Version at the bottom left corner
 
 
