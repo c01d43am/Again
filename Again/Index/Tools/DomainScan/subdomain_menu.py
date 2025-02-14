a70ba1200a7b858effcea0a8912abab846b87d1d
A Pawan <alugolupawan.com>
2025-02-14 10:42:15 +0530

    Initial commit

--- Again/Index/Tools/DomainScan/subdomain_menu.py
+++ Again/Index/Tools/DomainScan/subdomain_menu.py
@@ -1,8 +1,8 @@
-from Tools.DomainScan.sslscan_tool import check_and_install_sslscan, run_sslscan
+from Tools.DomainScan.sslscan_tool import check_and_install_sslscan
 from Tools.DomainScan.nmap_tool import check_and_install_nmap
 from Tools.DomainScan.feroxbuster_tool import is_feroxbuster_installed
 from Tools.DomainScan.gobuster_tool import is_tool_installed
-from Tools.DomainScan.dirb_tool import run_dirbuster  # Import the run_dirbuster function
+from Tools.DomainScan.dirb_tool import dirbuster_submenu  # Import the run_dirbuster function
 
 def subdomain_submenu():
     """Submenu to handle subdomain-related tasks."""
@@ -17,16 +17,10 @@ def subdomain_submenu():
         choice = input("Please choose an option (1-6): ")
         
         if choice == "1":
-            target_url = input("Enter the target URL for DirBuster: ")
-            wordlist = input("Enter wordlist path (default: /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt): ") or "/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt"
-            threads = input("Enter number of threads (default: 10): ") or "10"
-            file_ext = input("Enter file extensions to search (default: php,html,txt): ") or "php,html,txt"
-
-            run_dirbuster(target_url, wordlist, threads, file_ext)  # Call DirBuster with user input
-            
+            dirbuster_submenu()  # Call DirBuster with user input
         elif choice == "2":
             subdomain = input("Enter the subdomain to scan with SSLscan: ")
-            run_dirbuster(subdomain)
+            check_and_install_sslscan(subdomain)
         elif choice == "3":
             subdomain = input("Enter the subdomain to scan with Feroxbuster: ")
             is_feroxbuster_installed()
